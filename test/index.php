51431df93d5f1346289394ddf7aa516c30fc9428
hrib <hugo.dose@gmail.com>
2016-05-20 20:23:06 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -8,5 +8,16 @@ $tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 //var_dump($tinder->getUser());
 
 var_dump($tinder->updates());
+echo '<br>';
+
+echo '<table border="1" style="font-family:arial; font-size:7px;">';
+$response = $tinder->updates();
+foreach($response['matches'] as $match){
+      var_dump($match)
+      echo '<tr>';
+      echo '<td>' . $match['_id'] . '</td>';
+      echo '<td>';
+} 
+echo '</table>'
 
 ?>
