8a68bfa7878d250518f8f255d7ac1fee33bf3c8f
hrib <hugo.dose@gmail.com>
2016-05-20 19:58:37 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -1,7 +1,9 @@
 <?php
 require '../vendor/autoload.php';
 
-$tinder = new \Pecee\Http\Service\Tinder('1562221677425845', 'EAAGm0PX4ZCpsBAEWbJHNtOWYHHooTO12vp0ZBpbEUUGoeN3i4T4lueOsuzRF0UZCDPqzneH0t6rYZC7PLYFZB9chuh4QIsX7GzjjQZCz7tEcVWnOq833wPqScPHLLMR7vxDXOjGjFhMZBJguH89lUpL4PqbbtYwtEyBZBoaiMgjdxQZDZD');
+$fb_id = genenv('FB_ID');
+$token = genenv('FB_INDER_TOKEN');
+$tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 
 var_dump($tinder->getUser());
 
