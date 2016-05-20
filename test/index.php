4cf80fa2233b61447fcf12647fa515bfead04411
hrib <hugo.dose@gmail.com>
2016-05-20 20:45:50 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -5,7 +5,7 @@ $fb_id = getenv('FB_ID');
 $token = getenv('FB_INDER_TOKEN');
 $tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 
-var_dump($tinder->getUser());
+//var_dump($tinder->getUser());
 
 //var_dump($tinder->updates());
 echo '<br>';
@@ -14,14 +14,13 @@ echo '<br>';
 echo '<br>meio<br>';
 
 $response = $tinder->updates();
-var_dump($response->matches);
+//var_dump($response->matches);
 $item1 = $response->matches;
 echo '<br>';
 
 echo '<table border="1" style="font-family:arial; font-size:7px;">';
 foreach($item1 as $match){
       echo '<br>';
-      var_dump($match);
       echo '<tr>';
       echo '<td>' . $match['_id'] . '</td>';
       echo '<td>';
