f595a1c0da09dfc8f258b21cad5386d905e14dc4
hrib <hugo.dose@gmail.com>
2016-05-20 19:59:02 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -1,8 +1,8 @@
 <?php
 require '../vendor/autoload.php';
 
-$fb_id = genenv('FB_ID');
-$token = genenv('FB_INDER_TOKEN');
+$fb_id = getenv('FB_ID');
+$token = getenv('FB_INDER_TOKEN');
 $tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 
 var_dump($tinder->getUser());
