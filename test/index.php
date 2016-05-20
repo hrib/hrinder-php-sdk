6c04bfcf4b609f0e4a8a95e39c6d56e6cca16247
hrib <hugo.dose@gmail.com>
2016-05-20 21:07:30 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -6,7 +6,7 @@ $token = getenv('FB_INDER_TOKEN');
 $tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 
 //var_dump($tinder->getUser());
-$userId = '55501fb55d0076b064ed5b6f';
+$userId = '55501fb55d0076b064ed5b6f56c2eb45fdb7886152031092';
 $message = 'Plans for tonight?';
 $tinder->sendMessage($userId, $message);
 
