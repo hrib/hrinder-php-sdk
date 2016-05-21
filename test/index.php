376fe9f7b21c789229c08473b705338b45801191
hrib <hugo.dose@gmail.com>
2016-05-21 22:44:17 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -28,7 +28,7 @@ foreach($matches as $match){
             echo '<td>' . $mensagens->message . '</td>';
             echo '<td>' . $mensagens->sent_date . '</td>';
             echo '</tr>';
-            if (strpos($mensagens->message, 'izzzzzzzzzzzzzzz') !== false) {
+            if (strpos($mensagens->message, 'i') !== false) {
                   $mandou = 1;
             }
       }
