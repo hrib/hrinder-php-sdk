52faf69a19bae63dcc53071221f451ee4cb69903
hrib <hugo.dose@gmail.com>
2016-05-22 19:11:14 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -6,7 +6,7 @@ $token = getenv('FB_INDER_TOKEN');
 $tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 
 //var_dump($tinder->getUser());
-$userId = '55501fb55d0076b064ed5b6f56c2eb45fdb7886152031092';
+$myId = '56c2eb45fdb7886152031092';
 $texto = 'You should try my sensual massage: 1 hour relaxing massage with music, oil and candles... It might include sensual stimulation with you feel comfortable with. Contact me on instagram (@london_for_her) or facebook: Xmassage UK. :)';
 //var_dump($tinder->sendMessage($userId, $message));
 echo '<br>';
@@ -23,10 +23,17 @@ echo '<table border="1" style="font-family:arial; font-size:7px;">';
 foreach($matches as $match){
       $mandou = 0;
       foreach($match->messages as $mensagens){
+            
+            if($myId == $mensagens->from){$amigo = $mensagens->to; $esquerda = "";  $direita = $mensagens->message;}
+            if($myId == $mensagens->to){$amigo = $mensagens->from; $esquerda = $mensagens->message; $direita = "";}
+            
             echo '<tr>';
             echo '<td>' . $match->_id . '</td>';
-            echo '<td>' . $mensagens->message . '</td>';
             echo '<td>' . $mensagens->sent_date . '</td>';
+            echo '<td>' . $amigo . '</td>';
+            echo '<td>' . $esquerda . '</td>';
+            echo '<td>' . $direita . '</td>';
+            echo '<td>' . $myId . '</td>';
             echo '</tr>';
             if (strpos($mensagens->message, 'a') !== false) {
                   $mandou = 1;
