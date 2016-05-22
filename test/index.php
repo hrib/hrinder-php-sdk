d5981f29fdd1b9b95646e0e9bbf468ff76819c76
hrib <hugo.dose@gmail.com>
2016-05-22 22:39:27 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -36,7 +36,7 @@ foreach($matches as $match){
             echo '<td>' . $direita . '</td>';
             echo '<td>' . $myId . '</td>';
             echo '</tr>';
-            if (strpos($mensagens->message, 'azazaza') !== false) {
+            if (strpos($mensagens->message, 'a') !== false) {
                   $mandou = 1;
             }
       }
