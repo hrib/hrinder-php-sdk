82e2069b3b054b186adabe55b2c4c4a5e1c97f42
hrib <hugo.dose@gmail.com>
2016-05-23 17:18:00 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -8,7 +8,7 @@ $tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 //var_dump($tinder->getUser());
 $myId = '56c2eb45fdb7886152031092';
 //$texto = 'You should try my sensual massage: 1 hour relaxing massage with music, oil and candles... It might include sensual stimulation with you feel comfortable with. Contact me on instagram (@london_for_her) or facebook: Xmassage UK. :)';
-$texto = 'Pls if you want to know more, contact me on Instagram or Facebook. Thx :)';
+$texto = 'The therapy will take you into the heart and bloom of the flower of your femininity. An orgasm is not the goal, but rather sexual healing in whatever form it is expressed. The therapy is an opportunity to receive without any expectations. It is the absolute opportunity to experience the beauty and pleasure of sensual touch from another - totally as the receiver. Orgasmic delight is often experienced and has been described as ‘riding the wave’. Contacts: @london_for_her (instagram) or facebook.com/XmassageUK ';
 //var_dump($tinder->sendMessage($userId, $message));
 echo '<br>';
 echo '<br>';
