d69d082cd7d9945e9741a0237e1878e424f17ead
hrib <hugo.dose@gmail.com>
2016-05-23 20:09:22 +0100

    Create swipes.php

---
+++ test/swipes.php
@@ -0,0 +1,51 @@
+<?php
+require '../vendor/autoload.php';
+
+$fb_id = getenv('FB_ID');
+$token = getenv('FB_INDER_TOKEN');
+$tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
+
+//var_dump($tinder->getUser());
+$myId = '56c2eb45fdb7886152031092';
+//$texto = 'You should try my sensual massage: 1 hour relaxing massage with music, oil and candles... It might include sensual stimulation with you feel comfortable with. Contact me on instagram (@london_for_her) or facebook: Xmassage UK. :)';
+$texto = 'The therapy will take you into the heart and bloom of the flower of your femininity. An orgasm is not the goal, but rather sexual healing in whatever form it is expressed. The therapy is an opportunity to receive without any expectations. It is the absolute opportunity to experience the beauty and pleasure of sensual touch from another - totally as the receiver. Orgasmic delight is often experienced and has been described as ‘riding the wave’. Contacts: @london_for_her (instagram) or facebook.com/XmassageUK ';
+//var_dump($tinder->sendMessage($userId, $message));
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>meio<br>';
+
+$response = $tinder->updates();
+//var_dump($response->matches);
+$matches = $response->matches;
+echo '<br>';
+
+echo '<table border="1" style="font-family:arial; font-size:7px;">';
+foreach($matches as $match){
+      $mandou = 0;
+      foreach($match->messages as $mensagens){
+            
+            if($myId == $mensagens->from){$amigo = $mensagens->to; $esquerda = "";  $direita = $mensagens->message;}
+            if($myId == $mensagens->to){$amigo = $mensagens->from; $esquerda = $mensagens->message; $direita = "";}
+            
+            echo '<tr>';
+            echo '<td>' . $match->_id . '</td>';
+            echo '<td>' . $mensagens->sent_date . '</td>';
+            echo '<td>' . $amigo . '</td>';
+            echo '<td>' . $esquerda . '</td>';
+            echo '<td>' . $direita . '</td>';
+            echo '<td>' . $myId . '</td>';
+            echo '</tr>';
+            if (strpos($mensagens->message, 'zazaza') !== false) {
+                  $mandou = 1;
+            }
+      }
+      if($mandou == 0){
+            set_time_limit(10); 
+            sleep(1);
+            var_dump($tinder->sendMessage($match->_id, $texto));
+      }
+} 
+echo '</table>'
+
+?>
