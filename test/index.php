c857e996c26886d9cd0541c410a74d7113ad17a6
hrib <hugo.dose@gmail.com>
2016-05-25 15:49:11 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -3,14 +3,14 @@ require __DIR__ .'/../vendor/autoload.php';
 
 $fb_id = getenv('FB_ID');
 $token = getenv('FB_INDER_TOKEN');
-//$fb_id = getenv('FB_ID_MassTherr');
-//$token = getenv('FB_INDER_TOKEN_MassTherr');
+$fb_id = getenv('FB_ID_MassTherr');
+$token = getenv('FB_INDER_TOKEN_MassTherr');
 $tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 
 //var_dump($tinder->getUser());
 $myId = '56c2eb45fdb7886152031092';
 //$texto = 'You should try my sensual massage: 1 hour relaxing massage with music, oil and candles... It might include sensual stimulation with you feel comfortable with. Contact me on instagram (@london_for_her) or facebook: Xmassage UK. :)';
-$texto = 'The therapy will take you into the heart and bloom of the flower of your femininity. An orgasm is not the goal, but rather sexual healing in whatever form it is expressed. The therapy is an opportunity to receive without any expectations. It is the absolute opportunity to experience the beauty and pleasure of sensual touch from another - totally as the receiver. Orgasmic delight is often experienced and has been described as ‘riding the wave’. Contacts: @london_for_her (instagram) or facebook.com/XmassageUK ';
+$texto = 'The therapy will take you into the heart and bloom of the flower of your femininity. An orgasm is not the goal, but rather sexual healing in whatever form it is expressed. The therapy is an opportunity to receive without any expectations. It is the absolute opportunity to experience the beauty and pleasure of sensual touch from another - totally as the receiver. Orgasmic delight is often experienced and has been described as ‘riding the wave’.';
 //var_dump($tinder->sendMessage($userId, $message));
 echo '<br>';
 echo '<br>';
@@ -24,7 +24,7 @@ echo '<br>';
 
 echo '<table border="1" style="font-family:arial; font-size:7px;">';
 foreach($matches as $match){
-      $mandou = 1;
+      $mandou = 0;
       foreach($match->messages as $mensagens){
             
             if($myId == $mensagens->from){$amigo = $mensagens->to; $esquerda = "";  $direita = $mensagens->message;}
