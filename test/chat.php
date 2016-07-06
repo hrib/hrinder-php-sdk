cefe971c9398334fe9e5f4dff4ee1c2ee216cfdf
hrib <hugo.dose@gmail.com>
2016-07-06 13:08:00 +0100

    Update chat.php

--- test/chat.php
+++ test/chat.php
@@ -19,7 +19,7 @@ echo '<br>';
 foreach($matches as $match){
       echo $match->person->name;
       //echo $match->person->photos->processedFiles->url;
-      var_dump($match->person->photos);
+      var_dump($match->person->photos->url);
       echo '<table border="1" style="font-family:arial; font-size:7px;">';
       foreach($match->messages as $mensagens){
             
