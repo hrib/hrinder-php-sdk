2e181806a31f17bf3a18267277b1ddd6c713ed83
hrib <hugo.dose@gmail.com>
2016-07-06 16:55:32 +0100

    Update envia.php

--- test/envia.php
+++ test/envia.php
@@ -3,5 +3,10 @@ require __DIR__ .'/../vendor/autoload.php';
 echo $_GET["userid"]; 
 echo '<br>';
 echo $_GET["message"]; 
+echo '<br>';
+echo '<br>';
 var_dump($tinder->sendMessage($_GET["userid"], $_GET["message"]));
+echo '<br>';
+echo '<br>';
+echo 'fim';
 ?>
