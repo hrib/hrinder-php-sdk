1bba01285d681dd30ff1bb4c1851bef6216bf881
hrib <hugo.dose@gmail.com>
2016-07-06 14:45:18 +0100

    Update chat.php

--- test/chat.php
+++ test/chat.php
@@ -20,9 +20,10 @@ foreach($matches as $match){
       echo $match->person->name;
       //echo $match->person->photos->processedFiles->url;
       //var_dump($match->person->photos[0]->url);
-      foreach($match->person->photos as $foto){
-            echo '<img src='. $foto->url .' style="width:30px;height:30px;">';
-      }    
+      echo '<img src='. $match->person->photos[0]->url .' style="width:30px;height:30px;">';
+      //foreach($match->person->photos as $foto){
+      //      echo '<img src='. $foto->url .' style="width:30px;height:30px;">';
+      //}    
       echo '<table border="1" style="font-family:arial; font-size:7px;">';
       foreach($match->messages as $mensagens){
             
