28e5be9013574b5f2af2e96fd20c2a576f6da0dc
hrib <hugo.dose@gmail.com>
2016-07-07 10:54:16 +0100

    Update fb_login.php

--- test/fb_login.php
+++ test/fb_login.php
@@ -1,8 +1,12 @@
 <?php
 
+login();
+token();
+
+function login(){
 $login_email = getenv("email");
 $login_pass = getenv("passw");
-
+    
 //Simple cURL
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://m.facebook.com/login.php');
@@ -22,6 +26,9 @@ curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windo`enter code here`ws; U; W
 curl_setopt($ch, CURLOPT_REFERER, "http://m.facebook.com");
 $fbMain = curl_exec($ch) or die(curl_error($ch));
 var_dump($fbMain);
+}
+
+function token(){
 
 $url="https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token";
 $ch = curl_init();
@@ -33,12 +40,10 @@ $ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.16 (KHTML, like Gecko) \ 
 curl_setopt($ch, CURLOPT_USERAGENT, $ua);
 $a = curl_exec($ch); // $a will contain all headers
 $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // This is what you need, it will return you the last effective URL
-// Uncomment to see all headers
-/*
+
 echo "<pre>";
 print_r($a);echo"<br>";
 echo "</pre>";
-*/
 echo $url; // Voila
-
+}
 ?>
