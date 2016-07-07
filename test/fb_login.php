5f62e00118352d6c735c4c5544d7ff0920b9a516
hrib <hugo.dose@gmail.com>
2016-07-07 17:11:53 +0100

    Update fb_login.php

--- test/fb_login.php
+++ test/fb_login.php
@@ -26,9 +26,9 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windo`enter code here`ws; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3");
 curl_setopt($ch, CURLOPT_REFERER, "http://m.facebook.com");
 $fbMain = curl_exec($ch) or die(curl_error($ch));
-var_dump($fbMain);
+//var_dump($fbMain);
 
-echo '<br>.............1................<br>';
+//echo '<br>.............1................<br>';
 
 $url="https://m.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token";
 //$ch = curl_init();
@@ -48,22 +48,4 @@ echo $url; // Voila
 }
 
 
-function token(){
-
-$url="https://m.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Must be set to true so that PHP follows any "Location:" header
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.16 (KHTML, like Gecko) \ Chrome/24.0.1304.0 Safari/537.16';
-curl_setopt($ch, CURLOPT_USERAGENT, $ua);
-$a = curl_exec($ch); // $a will contain all headers
-$url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // This is what you need, it will return you the last effective URL
-
-echo "<pre>";
-print_r($a);echo"<br>";
-echo "</pre>";
-echo $url; // Voila
-}
 ?>
