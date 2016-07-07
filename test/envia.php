8a8b60e4b4bf7c15bfd7e7eba61f52e94bb4765f
hrib <hugo.dose@gmail.com>
2016-07-07 12:56:35 +0100

    Update envia.php

--- test/envia.php
+++ test/envia.php
@@ -1,27 +1,17 @@
 <?php
-//session_start();
-//require __DIR__ .'/../vendor/autoload.php';
-//$tinder = $_SESSION["hrinder"];
-//require __DIR__ .'/../vendor/autoload.php';
 
 require __DIR__ .'/../vendor/autoload.php';
 $fb_id = getenv('FB_ID_MassTherr6');
 $token = getenv('FB_INDER_TOKEN_MassTherr6');
 $tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 
-
 echo $_GET["userid"]; 
 echo '<br>';
 echo $_GET["message"]; 
 echo '<br>';
-echo '<br>';
 var_dump($tinder->sendMessage($_GET["userid"], $_GET["message"]));
 echo '<br>';
-echo '<br>';
-echo 'fim1';
-echo '<br>';
-header("Location: chat.php"); 
+header("Location: chat.php#'.$_GET["userid"].'"); 
 exit;
-echo '<br>';
-echo 'fim2';
+
 ?>
