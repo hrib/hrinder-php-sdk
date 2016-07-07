f46a497ca1df6d6a100b0b875d9fa9f12be42a8e
hrib <hugo.dose@gmail.com>
2016-07-07 19:10:03 +0100

    Update chat.php

--- test/chat.php
+++ test/chat.php
@@ -31,8 +31,9 @@ foreach($matches as $match){
       //}  
       //$num =  $num - 1;
       $max = count($match->messages) - 1;
-      echo $max;
-      echo '<div id =' . $match->_id . ' class="test" data-percentage=' . $match->messages[$max]->sent_date . ' style="width:702px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;">';
+      $timestamp = strtotime($match->messages[$max]->sent_date);
+      echo '<div id =' . $match->_id . ' class="test" data-percentage=' . $timestamp . ' style="width:702px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;">';
+      echo $timestamp . ':' . $max;
       echo '<img src='. $match->person->photos[0]->url .' style="width:30px;height:30px;">';
       echo  '<font style="font-family:arial; font-size:9px;"> ' . $match->person->name . '</font>';
       echo '<div name =table style="height:150px;width:700px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">';
