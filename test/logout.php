795ec60aadae24d38ea61d1b824f508d9ddac3da
hrib <hugo.dose@gmail.com>
2016-07-08 10:51:02 +0100

    Update logout.php

--- test/logout.php
+++ test/logout.php
@@ -1,7 +1,28 @@
 <?php
 session_start();
-session_unset($_SESSION["token"]);
+//From PHP manual: Unset all of the session variables.
+//No need to do in a loop for all $_SESSION[] keys
+$_SESSION = array();
+
+//For cookies you do similar, from PHP docs:
+//http://php.net/manual/en/function.setcookie.php#73484
+
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
 session_destroy();
+
+
+
+
+
+
 header("location:login.php");
 exit();
 
