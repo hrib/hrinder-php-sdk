b901e17fb42e7de46093bee723404872d8ab91ba
hrib <hugo.dose@gmail.com>
2016-07-08 18:07:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ html {
 }
 div.transbox {
   margin: 30px;
-  width: 400px;
-  height: 300px;
+  width: 350px;
+  height: 170px;
   border: 1px solid black;
   background-color: rgba(255,255,255,.5);
   position: absolute;
@@ -34,11 +34,11 @@ div.transbox {
       <table>
       <tr valign="bottom">
       <td><font style="font-family:arial; font-size:11px;">Email or Phone </font></td>
-      <td><input type="text" name="user" style="font-family:arial; font-size:11px; width: 180px; margin-left: 0px; margin-top: 10px;"></td>
+      <td><input type="text" name="user" style="font-family:arial; font-size:10px; width: 180px; margin-left: 0px; margin-top: 10px;"></td>
       </tr>
       <tr valign="bottom">
       <td><font style="font-family:arial; font-size:9px;">Password </font></td>
-      <td><input type="password" name="password" style="font-family:arial; font-size:9px; width: 180px; margin-left: 0px; margin-top: 10px;"></td>
+      <td><input type="password" name="password" style="font-family:arial; font-size:10px; width: 180px; margin-left: 0px; margin-top: 10px;"></td>
       </tr>
       <tr valign="bottom" valign="left">
       <td></td>
