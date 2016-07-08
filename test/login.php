19e14a8dd6d4cdb039e4ce009a0cbb32f74fa191
hrib <hugo.dose@gmail.com>
2016-07-08 08:34:17 +0100

    Create login.php

---
+++ test/login.php
@@ -0,0 +1,10 @@
+<?php
+
+
+      echo '<form action="fb_login.php" method="post">';
+      echo '<input type="text" name="user" style="font-family:arial; font-size:7px; width: 600px; margin-left: 10px; margin-top: 10px;">';
+      echo '<input type="text" name="password" style="font-family:arial; font-size:7px; width: 600px; margin-left: 10px; margin-top: 10px;">';
+      echo '<input type="submit" value="Send" style="font-family:arial; font-size:7px; margin-left: 10px; margin-top: 10px;">';
+      echo '</form>'; 
+      
+?>
