b29b14fcf00b9a71c15d97784c5e482ef8621d6d
hrib <hugo.dose@gmail.com>
2016-07-08 13:07:27 +0100

    Update login.php

--- test/login.php
+++ test/login.php
@@ -4,7 +4,7 @@ unlink('mirazmac_cookie.txt');
 
       echo '<form action="fb_login.php" method="post">';
       echo '<input type="text" name="user" style="font-family:arial; font-size:7px; width: 600px; margin-left: 10px; margin-top: 10px;">';
-      echo '<input type="text" name="password" style="font-family:arial; font-size:7px; width: 600px; margin-left: 10px; margin-top: 10px;">';
+      echo '<input type="password" name="password" style="font-family:arial; font-size:7px; width: 600px; margin-left: 10px; margin-top: 10px;">';
       echo '<input type="submit" value="Send" style="font-family:arial; font-size:7px; margin-left: 10px; margin-top: 10px;">';
       echo '</form>'; 
       
