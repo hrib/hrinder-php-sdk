3c7006ba92d65c1826868a7abd2c09b183edfa5e
hrib <hugo.dose@gmail.com>
2016-07-08 11:53:11 +0100

    Update envia.php

--- test/envia.php
+++ test/envia.php
@@ -12,7 +12,9 @@ echo $_GET["message"];
 echo '<br>';
 var_dump($tinder->sendMessage($_GET["userid"], $_GET["message"]));
 echo '<br>';
-header('Location: chat.php#' . $_GET["userid"]); 
+//header('Location: chat.php#' . $_GET["userid"]); 
+header('Location: chat.php'); 
+
 exit;
 
 ?>
