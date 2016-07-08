25724c1344f74bc2f324a3302e3a5afcd0100aff
hrib <hugo.dose@gmail.com>
2016-07-08 11:29:24 +0100

    Update fb_login.php

--- test/fb_login.php
+++ test/fb_login.php
@@ -3,12 +3,12 @@ session_start();
 session_regenerate_id();
 
 $token = login();
-echo '<br>';
-echo $token;
-echo '<br>';
+//echo '<br>';
+//echo $token;
+//echo '<br>';
 $_SESSION["token"] = $token;
-echo $_SESSION["token"];
-echo '<br>';
+//echo $_SESSION["token"];
+//echo '<br>';
 header('Location: chat.php'); 
 //header('Location: chat.php?tk=' . $token); 
 exit;
@@ -53,12 +53,12 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $a = curl_exec($ch); // $a will contain all headers
 $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // This is what you need, it will return you the last effective URL
 
-echo "<pre>";
-print_r($a);
-echo"<br>";
-echo "</pre>";
-echo $url; // Voila
-echo"<br>";
+//echo "<pre>";
+//print_r($a);
+//echo"<br>";
+//echo "</pre>";
+//echo $url; // Voila
+//echo"<br>";
 curl_close($ch);
 $pos1 = strpos($a, "access_token=") + 13;
 $pos2 = strpos($a, "&expires_in");
