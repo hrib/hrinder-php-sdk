f7863bc742ea08700f7664fab50ef698d97e4e35
hrib <hugo.dose@gmail.com>
2016-07-12 16:26:51 +0100

    Update fb_login.php

--- fb_login.php
+++ fb_login.php
@@ -45,16 +45,16 @@ $fbMain = curl_exec($ch) or die(curl_error($ch));
 
 
 $pos1a = strpos($fbMain, "fb_dtsg") + 16;
-$pos2a = strpos(substr($fbMain,$pos1a+50), "autocomplete") - 2;
+$pos2a = strpos(substr($fbMain,$pos1a,50), "autocomplete") - 2;
 $fb1 = substr($fbMain,$pos1a,$pos2a - $pos1a);
 echo $fb1;
 echo '<br>';
 $pos1b = strpos($fbMain, "charset_test") + 21;
-$pos2b = strpos(substr($fbMain,$pos1b+50), "/>") - 2;
+$pos2b = strpos(substr($fbMain,$pos1b,50), "/>") - 2;
 $fb2 = substr($fbMain,$pos1b,$pos2b - $pos1b);
 echo $fb2;
 echo '<br>';
-echo $fbMain;
+//echo $fbMain;
 //var_dump($fbMain);
 
 //echo '<br>.............1................<br>';
