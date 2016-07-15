2dc93acead669d0733e4a0610f5393564ea5b33d
hrib <hugo.dose@gmail.com>
2016-07-15 11:44:18 +0100

    Update get-facebook.php

--- get-facebook.php
+++ get-facebook.php
@@ -6,11 +6,13 @@ $varin1 = getenv('email');
 $varin2 = getenv('pass');
 //$stdOut = exec(sprintf('%s %s %s %s', $pathToPhatomJs,  $pathToJsScript, $varin1, $varin2), $out);
 $stdOut = exec(sprintf('%s %s %s %s %s', $pathToPhatomJs, '--proxy=157.7.152.61:3128', $pathToJsScript, $varin1, $varin2), $out);
-
 //$stdOut = exec(sprintf('%s %s', $pathToPhatomJs,  $pathToJsScript), $out);
 echo $stdOut;
 
-    
+
+$pathToJsScript = dirname(__FILE__). '/browser_ip.js';
+$stdOut = exec(sprintf('%s %s %s', $pathToPhatomJs, '--proxy=157.7.152.61:3128', $pathToJsScript), $out);
+echo $stdOut; 
     
     
 ?>
