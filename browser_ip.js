9dc5923cfd95d29f7a923acf18f1be099c144fe1
hrib <hugo.dose@gmail.com>
2016-07-15 09:04:13 +0100

    Update browser_ip.js

--- browser_ip.js
+++ browser_ip.js
@@ -10,18 +10,18 @@ page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
     //var resultingHtml = page.evaluate(function() {
     //    return document.title;
     //});
-    page.render('aaa.png');
+    page.render('ip1.png');
     //console.log(resultingHtml);
 
     
     setTimeout(function(){
-        page.render('1.png');
+        page.render('ip2.png');
         //console.log('print');
     }, 5000);
 
 
     setTimeout(function(){
-        page.render('2.png');
+        page.render('ip3.png');
         var resHtml = page.evaluate(function() {
             return document.documentElement.innerHTML;
         });
