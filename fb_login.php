6454565a82cf37b5a7836fe2ada90b12edf6446b
hrib <hugo.dose@gmail.com>
2016-07-15 09:13:16 +0100

    Update fb_login.php

--- fb_login.php
+++ fb_login.php
@@ -18,81 +18,23 @@ if($token == "invalid"){
 exit;
 
 function login(){
-//$login_email = getenv("email");
-//$login_pass = getenv("passw");
+
 $login_email = $_POST["user"];
 $login_pass = $_POST["password"];
 
-    
-//Simple cURL
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://m.facebook.com/login.php');
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'charset_test=â‚¬,Â´,â‚¬,Â´,æ°´,Ð”,Ð„&email=' . urlencode($login_email) . '&pass=' . urlencode($login_pass) . '&login=Login');
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept-Charset: utf-8',
-    'Accept-Language: en-us,en;q=0.7,bn-bd;q=0.3',
-    'Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5'));
-curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd() . '/mirazmac_cookie.txt'); // The cookie file
-curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd() . '/mirazmac_cookie.txt'); // cookie jar
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windo`enter code here`ws; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3");
-curl_setopt($ch, CURLOPT_REFERER, "http://m.facebook.com");
-$fbMain = curl_exec($ch) or die(curl_error($ch));
-var_dump($fbMain);
+$pathToPhatomJs = 'bin/phantomjs';
+$pathToJsScript = dirname(__FILE__). '/browser_fb.js';
+$varin1 = $login_email;
+$varin2 = $login_pass;
+$stdOut = exec(sprintf('%s %s %s %s', $pathToPhatomJs,  $pathToJsScript, $varin1, $varin2), $out);
+echo $stdOut;
 
-echo '<br>........................<br>';
-$pos1a = strpos($fbMain, "fb_dtsg") + 16;
-echo 'pos1a: '. $pos1a .'<br>';
-$pos2a = strpos(substr($fbMain,$pos1a,100), "autocomplete") - 2 + $pos1a;
-echo 'pos2a: '. $pos2a .'<br>';
-$fb1 = substr($fbMain,$pos1a,$pos2a - $pos1a);
-echo $fb1;
-echo '<br>........................<br>';
-$pos1b = strpos($fbMain, "charset_test") + 21;
-echo 'pos1b: '. $pos1b .'<br>';
-$pos2b = strpos(substr($fbMain,$pos1b,100), "/>") - 2 + $pos1b;
-echo 'pos2b: '. $pos2b .'<br>';
-$fb2 = substr($fbMain,$pos1b,$pos2b - $pos1b);
-echo $fb2;
-echo '<br>........................<br>';
-//echo $fbMain;
+$pos1 = strpos($stdOut, "access_token=") + 13;
+$pos2 = strpos($stdOut, "&expires_in");
+$token = substr($stdOut,$pos1,$pos2 - $pos1);
+echo $token;
 
-curl_setopt($ch, CURLOPT_URL, 'https://m.facebook.com/login/checkpoint/');
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'charset_test=' . $fb2. '&fb_dtsg=' . $fb1 );
-$fbCheck = curl_exec($ch) or die(curl_error($ch));
-var_dump($fbCheck);
-
-
-//echo '<br>.............1................<br>';
-/*
-$url="https://m.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token";
-//$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Must be set to true so that PHP follows any "Location:" header
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//$ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.16 (KHTML, like Gecko) \ Chrome/24.0.1304.0 Safari/537.16';
-//curl_setopt($ch, CURLOPT_USERAGENT, $ua);
-$a = curl_exec($ch); // $a will contain all headers
-$url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // This is what you need, it will return you the last effective URL
-
-//echo "<pre>";
-//print_r($a);
-//echo"<br>";
-//echo "</pre>";
-//echo $url; // Voila
-//echo"<br>";
-curl_close($ch);
-$pos1 = strpos($a, "access_token=") + 13;
-$pos2 = strpos($a, "&expires_in");
-$token = substr($a,$pos1,$pos2 - $pos1);
-//echo $token;
 return $token;
-*/
 }
 
 
