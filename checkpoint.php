3cfd86b1b9b0b32a61205379ffb5b4033b861070
hrib <hugo.dose@gmail.com>
2016-07-18 14:09:06 +0100

    Update checkpoint.php

--- checkpoint.php
+++ checkpoint.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
 
-echo 'checkpoint';
-echo $_SESSION["user"];
-echo $_SESSION["password"];
-echo $_SESSION["token"];
+echo 'checkpoint<br>';
+//echo $_SESSION["user"];
+//echo $_SESSION["password"];
+//echo $_SESSION["token"];
+echo $_POST["fb_dtsg"] .'<br>';
+echo $_POST["nh"] .'<br>';
 echo '<br>..........................<br>';
 echo $_SESSION["checkpoint"];
 
