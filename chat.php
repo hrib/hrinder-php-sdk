cb94ee3268e59d0d0a4549817b5cb6f92ab270c0
hrib <hugo.dose@gmail.com>
2017-01-13 15:27:32 +0000

    Update chat.php

--- chat.php
+++ chat.php
@@ -13,8 +13,11 @@ require __DIR__ .'/vendor/autoload.php';
 
 
 //$fb_id = getenv('FB_ID_MassTherr6');
-$token = getenv('FB_INDER_TOKEN');
+
 //$token = $_SESSION["token"];
+$token = getenv('FB_INDER_TOKEN');
+$_SESSION["token"] = $token;
+
 //$token = $_GET["tk"];
 //$fb_id = $_GET["fb"];
 
