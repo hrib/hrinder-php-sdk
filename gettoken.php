ff7120659ad7462942edadd6ef0d5655b2a17c81
hrib <hugo.dose@gmail.com>
2017-03-08 13:51:46 +0000

    Update gettoken.php

--- gettoken.php
+++ gettoken.php
@@ -47,7 +47,7 @@ $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // This is what you need, it w
 //echo"<br>";
 curl_close($ch);
 //////////////////////////////////////////////////////////////////////////////////
-$url="https://www.facebook.com/v2.6/dialog/oauth/confirm?dpr=0.89552241563797";
+$url="https://www.facebook.com/v2.6/dialog/oauth/confirm?dpr=0.89552241563797&redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&state=%7B%22challenge%22%3A%22q1WMwhvSfbWHvd8xz5PT6lk6eoA%253D%22%2C%220_auth_logger_id%22%3A%2254783C22-558A-4E54-A1EE-BB9E357CC11F%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=54783C22-558A-4E54-A1EE-BB9E357CC11F#_=";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, true);
