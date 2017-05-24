39c560191215378b0e6a5075d8f6a1905a2f3586
hrib <hugo.dose@gmail.com>
2017-05-24 11:42:21 +0100

    Update envia.php

--- envia.php
+++ envia.php
@@ -11,12 +11,24 @@ $tinder = new \Pecee\Http\Service\Tinder($fb_id, $token);
 echo $_GET["userid"]; 
 echo '<br>';
 echo $_GET["message"]; 
+$mensagem = desesconde($_GET["message"]);
 echo '<br>';
-var_dump($tinder->sendMessage($_GET["userid"], $_GET["message"]));
+var_dump($tinder->sendMessage($_GET["userid"], $mensagem));
 echo '<br>';
 //header('Location: chat.php#' . $_GET["userid"]); 
 header('Location: chat.php'); 
 
 exit;
+
+
+function desesconde($texto){
+      $texto = str_replace('£1£','a',$texto);
+      $texto = str_replace('£2£','e',$texto);
+      $texto = str_replace('£3£','i',$texto);
+      $texto = str_replace('£4£','o',$texto);
+      $texto = str_replace('£5£','u',$texto);
+      
+      return $texto;
+}
 
 ?>
