12b89c281103b9ad8bc03496b3f21c624f2ca421
hrib <hugo.dose@gmail.com>
2017-10-29 22:31:44 +0000

    Update chat.php

--- chat.php
+++ chat.php
@@ -115,11 +115,11 @@ function esconde($texto){
 
       
       $( document ).ready(function() {
-            replaceText('£1£', 'a', 'g');
-            replaceText('£2£', 'e', 'g');
-            replaceText('£3£', 'i', 'g');
-            replaceText('£4£', 'o', 'g');
-            replaceText('£5£', 'u', 'g');
+            //replaceText('£1£', 'a', 'g');
+            //replaceText('£2£', 'e', 'g');
+            //replaceText('£3£', 'i', 'g');
+            //replaceText('£4£', 'o', 'g');
+            //replaceText('£5£', 'u', 'g');
             
             var $wrapper = $('.testWrapper');
             $wrapper.find('.test').sort(function (a, b) {
