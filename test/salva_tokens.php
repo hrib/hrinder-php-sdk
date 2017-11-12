8e8f244d978b527187dc9787aa440e846dec3c65
hrib <hugo.dose@gmail.com>
2017-11-12 17:26:43 +0000

    Update salva_tokens.php

--- test/salva_tokens.php
+++ test/salva_tokens.php
@@ -14,30 +14,26 @@ $id = $_POST['id'];
 
 <?php
 if (isset($token)) { 
-    grava($token, $id);
-}
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+    $dsn = "pgsql:"
+        . "host=" . $dbopts["host"] . ";"
+        . "dbname=". ltrim($dbopts["path"],'/') . ";"
+        . "user=" . $dbopts["user"] . ";"
+        . "port=" . $dbopts["port"] . ";"
+        . "sslmode=require;"
+        . "password=" . $dbopts["pass"];
 
-function grava(token, id){
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$dsn = "pgsql:"
-    . "host=" . $dbopts["host"] . ";"
-    . "dbname=". ltrim($dbopts["path"],'/') . ";"
-    . "user=" . $dbopts["user"] . ";"
-    . "port=" . $dbopts["port"] . ";"
-    . "sslmode=require;"
-    . "password=" . $dbopts["pass"];
-    
-$db = new PDO($dsn);
-$tempo = date('m/d/Y h:i:s a');
-$query = "INSERT INTO tl_usuarios (tempo, t_id, t_token) VALUES ('" . $tempo . "', '" . $id . "', '" . $token . "');";
-//echo var_dump($query);
-//echo '<br><br>';
-$result = $db->query($query);
-echo var_dump($result);
-//echo '<br><br>';
-$result->closeCursor();
-//$app->register(new Herrera\Pdo\PdoServiceProvider(), $zica);
-//echo '<br>end<br>';
+    $db = new PDO($dsn);
+    $tempo = date('m/d/Y h:i:s a');
+    $query = "INSERT INTO tl_usuarios (tempo, t_id, t_token) VALUES ('" . $tempo . "', '" . $id . "', '" . $token . "');";
+    //echo var_dump($query);
+    //echo '<br><br>';
+    $result = $db->query($query);
+    echo var_dump($result);
+    //echo '<br><br>';
+    $result->closeCursor();
+    //$app->register(new Herrera\Pdo\PdoServiceProvider(), $zica);
+    //echo '<br>end<br>';
 }
 
 ?>
